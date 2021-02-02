0a456d2733c30e7d4dee239128a3f8ebe37871c6
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-02 17:15:38 +0530

    updated files

--- react-basics/src/components/NameList.js
+++ react-basics/src/components/NameList.js
@@ -25,7 +25,7 @@ function NameList() {
     const names = ['Bruce', 'Clark', 'Diana', 'Clark']
     // key is only for element stability. here index is used as key as index of every element is unique so we have to 
     //     pass 2nd parameter as index in arrow function.
-    const nameList = names.map((name,index) => <h2 key={index}>{index} {name}</h2>)
+    const nameList = names.list.map((name,index) => <h2 key={index}>{index} {name}</h2>)
     
     return <div>{nameList}</div>
 }
