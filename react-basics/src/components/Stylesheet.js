8878fc08e83349ce95687690d40e076f094dfb83
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-03 15:30:46 +0530

    stylesheets

---
+++ react-basics/src/components/Stylesheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Mystyles.css'
+
+function Stylesheet() {
+    return (
+        <div>
+            <h2 className="primary">StyleSheet</h2>
+        </div>
+    )
+}
+
+export default Stylesheet
