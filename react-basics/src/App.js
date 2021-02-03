436e67221b2d548f4c166ae64246f9157c1f4703
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-03 21:06:17 +0530

    updated styling

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <div className="App">
         <h1 className="error">Error</h1>
         <h1 className={styles.success}>Success</h1>
-        {/* <Inline /> */}
+        <Inline />
         {/* <Stylesheet primary = {true}/> */}
         {/* <NameList /> */}
         {/* <UserGreeting /> */}
--- react-basics/src/components/Inline.js
+++ react-basics/src/components/Inline.js
@@ -8,6 +8,7 @@ const heading = {
 function Inline() {
     return (
         <div style={heading}>
+            <h1 className="error">Error</h1>
             <h1>Inline</h1>
         </div>
     )
