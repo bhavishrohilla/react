f2750f9a7f7f67660615bcf409d1a810c95947ce
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-04 12:31:49 +0530

    comment handler

--- react-basics/src/components/Form.js
+++ react-basics/src/components/Form.js
@@ -6,7 +6,8 @@ export class Form extends Component {
         super(props)
     
         this.state = {
-             username : ''
+             username : '',
+             comments : ''
         }
     }
     //when you assign a handler onChange event, the event itself is passed to the parameter to the handler
@@ -15,13 +16,22 @@ export class Form extends Component {
             username: event.target.value
         })
     }
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
 
     render() {
         return (
             <form>
                 <div>
                     <label>Username</label>
-                    <input tpe="text" value={this.state.username} onChange={this.handleUsernameChange}/>
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange}/>
+                </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}/>
                 </div>
             </form>
         )
